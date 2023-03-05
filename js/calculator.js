a74d5c05b492333e444d11be83d98738da402e94
mmracho <20000699+mmracho@users.noreply.github.com>
2023-03-05 11:22:20 +0800

    Add basic calculator functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,1 +1,32 @@
 console.log('Hello, world');
+
+function addNumbers(num1, num2) {
+  return;
+}
+
+function subtractNumbers(num1, num2) {
+  return;
+}
+
+function multiplyNumbers(num1, num2) {
+  return;
+}
+
+function divideNumbers(num1, num2) {
+  return;
+}
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return addNumbers(num1, num2);
+    case '-':
+      return subtractNumbers(num1, num2);
+    case '*':
+      return multiplyNumbers(num1, num2);
+    case '/':
+      return divideNumbers(num1, num2);
+    default:
+      return 'OOPS';
+  }
+}
